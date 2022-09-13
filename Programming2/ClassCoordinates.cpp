cc786021274d23f6c15be9e4b28a7c8964a55fd2
Andrey Karagodin <shad-karagodin@ya.ru>
2022-09-13 17:29:52 +0300

    fix code style

--- Programming2/ClassCoordinates.cpp
+++ Programming2/ClassCoordinates.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 Karagodin Andrey Romanovich. All rights reserved.
 #include <iostream>
 
 template <class Type> class Coordinates;
@@ -5,47 +6,47 @@ template <class Type> std::ostream& operator <<(std::ostream& out, const Coordin
 
 template <class Type>
 class Coordinates {
-	Type first;
-	Type second;
+    Type first;
+    Type second;
+
 public:
+    Coordinates() : first(0), second(0) {}
 
-	Coordinates() : first(0), second(0) {}
+    Coordinates(Type _first, Type _second) {
+        first = _first;
+        second = _second;
+    }
 
-	Coordinates(Type _first, Type _second) {
-		 first = _first;
-		 second = _second;
-	}
+    Coordinates(const Coordinates& other) {
+        first = other.first;
+        second = other.second;
+    }
 
-	Coordinates(const Coordinates& other) {
-		first = other.first;
-		second = other.second;
-	}
+    void print() {
+        std::cout << "(" << first << ", " << second << ")" << std::endl;
+    }
 
-	void print() {
-		std::cout << "(" << first << ", " << second << ")" << std::endl;
-	}
+    Coordinates& operator+(const Coordinates& n) {
+        Coordinates res;
+        res.first = first + n.first;
+        res.second = second + n.second;
+        return res;
+    }
 
-	Coordinates& operator+(const Coordinates& n) {
-		Coordinates res;
-		res.first = first + n.first;
-		res.second = second + n.second;
-		return res;
-	}
+    Coordinates& operator=(const Coordinates n) {
+        if (this == &n) {
+            return *this;
+        }
+        first = n.first;
+        second = n.second;
+        return *this;
+    }
 
-	Coordinates& operator=(const Coordinates n) {
-		if (this == &n) {
-			return *this;
-		}
-		first = n.first;
-		second = n.second;
-		return *this;
-	}
-
-	friend std::ostream& operator<< <Type>(std::ostream& out, const Coordinates& r);
+    friend std::ostream& operator<< <Type>(std::ostream& out, const Coordinates& r);
 };
 
 template <class Type>
 std::ostream& operator <<(std::ostream& out, const Coordinates<Type>& r) {
-	out << "(" << n.first << ", " << n.second << ")" << std::endl;
-	return out;
+    out << "(" << n.first << ", " << n.second << ")" << std::endl;
+    return out;
 }
--- Programming2/main.cpp
+++ Programming2/main.cpp
@@ -1,11 +1,12 @@
+// Copyright 2022 Karagodin Andrey Romanovich. All rights reserved.
 #include "ClassCoordinates.cpp"
 
 
 int main() {
-	Coordinates<float> A(5.4, 4.2), B(3.1, 5.8), C(A);
-	A.print();
-	B.print();
-	C = A + B;
-	std::cout << C << std::endl;
-	return 0;
+    Coordinates<float> A(5.4, 4.2), B(3.1, 5.8), C(A);
+    A.print();
+    B.print();
+    C = A + B;
+    std::cout << C << std::endl;
+    return 0;
 }
