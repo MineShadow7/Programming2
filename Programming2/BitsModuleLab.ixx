02aa7a4809cb8a811134d536813d3786e0b547e1
Andrey Karagod <shad-karagodin@ya.ru>
2022-09-28 12:19:23 +0300

    Trying out modules
    Ничего не понял, но очень интересно. На лекции чуть пересмотрю как с ними работать.

---
+++ Programming2/BitsModuleLab.ixx
@@ -0,0 +1,7 @@
+export module BitsModuleLab;
+
+
+export void myfunc() {
+
+}
+
--- Programming2/main.cpp
+++ Programming2/main.cpp
@@ -1,25 +1,6 @@
-// Copyright 2022 Karagodin Andrey Romanovich. All rights reserved.
-#include <iostream>
-#include <vector>
-
-enum status {
-	Private,
-	Public,
-	Protected
-};
+import BitsModuleLab;
+import std.core;
 
 int main() {
-	std::vector<std::pair<int, float>> classTest;
-	for (int i = 0; i < 10; i++) {
-		std::pair<int, float> newpair;
-		newpair.first = rand() % 3;
-		newpair.second = rand() % 10;
-		classTest.push_back(newpair);
-	}
 
-	for (std::vector<std::pair<int, float>>::iterator it = begin(classTest); it != end(classTest); ++it) {
-		std::cout << (*it).first << " " << (*it).second << std::endl;
-	}
-
-	return 0;
 }
