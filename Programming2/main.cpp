3d843bd7e3075875d031e01bc78a1b9f6fc90a73
Andrey Karagodin <shad-karagodin@ya.ru>
2022-10-05 23:50:53 +0300

    Matrix Work done

--- Programming2/main.cpp
+++ Programming2/main.cpp
@@ -1,25 +1,143 @@
 // Copyright 2022 Karagodin Andrey Romanovich. All rights reserved.
-#include <iostream>
-#include <vector>
+#include <vector>;
+#include <iostream>;
 
-enum status {
-	Private,
-	Public,
-	Protected
-};
+void print(std::vector<std::vector<int>> matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++)
+            std::cout << matrix[i][j] << " ";
+        std::cout << std::endl;
+    }
+}
+
+std::vector<std::vector<int>> set(int size, std::vector<std::vector<int>> matrix) {
+    std::vector<std::vector<int>> matrix2(size, std::vector<int>(size, 0));
+    std::cout << "Enter new matrix" << std::endl;
+    for (int i = 0; i < matrix.size(); i++) {
+        std::cout << std::endl << "New row:" << std::endl;
+        for (int j = 0; j < matrix2[i].size(); j++) {
+            std::cin >> matrix2[i][j];
+            std::cout << " ";
+        }
+    }
+    std::cout << "Printing your new matrix: " << std::endl;
+    print(matrix2);
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix2[i].size(); j++) {
+            matrix[i][j] = matrix2[i][j];
+        }
+    }
+    return matrix;
+}
+
+std::vector<std::vector<int>> operator-(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b)
+{
+    if (a.size() != b.size())
+        throw("a.size() != b.size()");
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> operator+(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b)
+{
+    if (a.size() != b.size())
+        throw("a.size() != b.size()");
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> operator*(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b)
+{
+    if (a.size() != b.size())
+        throw("a.size() != b.size()");
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[i][j] * b[j][i];
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> operator*(const std::vector<std::vector<int>>& a, const int b)
+{
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[i][j] * b;
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> operator*(const std::vector<std::vector<int>>& a, const std::vector<int>& b)
+{
+    if (a.size() != b.size())
+        throw("a.size() != b.size()");
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[i][j] * b[j];
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> transp(std::vector<std::vector<int>>& a) {
+    std::vector<std::vector<int>> c(a.size(), std::vector<int>(a.size(), 0));
+    for (size_t i = 0; i < a.size(); ++i) {
+        for (size_t j = 0; j < a[i].size(); j++) {
+            c[i][j] = a[j][i];
+        }
+    }
+    return c;
+}
 
 int main() {
-	std::vector<std::pair<int, float>> classTest;
-	for (int i = 0; i < 10; i++) {
-		std::pair<int, float> newpair;
-		newpair.first = rand() % 3;
-		newpair.second = rand() % 10;
-		classTest.push_back(newpair);
-	}
+    int size = 5;
+    std::vector<std::vector<int>> matrix(size, std::vector<int>(size, 0));
+    print(matrix);
+    matrix = set(size, matrix);
 
-	for (std::vector<std::pair<int, float>>::iterator it = begin(classTest); it != end(classTest); ++it) {
-		std::cout << (*it).first << " " << (*it).second << std::endl;
-	}
+    std::cout << "Matrix = Matrix 2" << std::endl;
+    std::vector<std::vector<int>> matrix2(size, std::vector<int>(size, 0));
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        for (size_t j = 0; j < matrix[i].size(); j++) {
+            matrix2[i][j] = matrix[i][j];
+        }
+    }
 
-	return 0;
+    std::cout << "Operator +" << std::endl;
+    matrix = matrix + matrix2;
+    print(matrix);
+    std::cout << "Operator -" << std::endl;
+    matrix = matrix - matrix2;
+    print(matrix);
+    std::cout << "Operator * on matrix" << std::endl;
+    matrix = matrix * matrix2;
+    print(matrix);
+    std::cout << "Operator * on a number" << std::endl;
+    matrix = matrix * 6;
+    print(matrix);
+
+    std::vector<int> vec{ 6, 7, 8, 9, 10 };
+    std::cout << "Operator * on a vector" << std::endl;
+    matrix = matrix * vec;
+    print(matrix);
+
+    std::cout << "Transponirovanie matrix" << std::endl;
+    matrix = transp(matrix);
+    print(matrix);
+
+
 }
